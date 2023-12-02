db63c854f1c196871936ca302ac58f5180efbaed
Huyền Trang <98087513+PhamVuHuyenTrang@users.noreply.github.com>
2023-12-02 22:45:32 +0700

    Update icarl_lipschitz.py

--- models/icarl_lipschitz.py
+++ models/icarl_lipschitz.py
@@ -315,7 +315,7 @@ class ICarlLipschitz(RobustnessOptimizer):
                 bf = torch.cat([bf] * (af.shape[0] // bf.shape[0]))
             
                 distance = torch.sqrt(((bf - af) ** 2).sum(axis=(1, 2, 3)))
-                loss_lr += distance.sum()
+                loss_lr += reg * distance.sum()
 
         # print(f'loss ce: {loss_ce}, loss wd: {loss_wd}, loss_lr: {loss_lr}')
         loss = loss_ce + loss_wd + loss_lr

804ddc7372e507c19203ef2edfec4fac141c3874
Huyền Trang <98087513+PhamVuHuyenTrang@users.noreply.github.com>
2023-12-03 09:17:18 +0700

    Update buffer.py

--- functions/buffer.py
+++ functions/buffer.py
@@ -5,8 +5,7 @@ from typing import Tuple
 from torch.functional import Tensor
 from torchvision import transforms
 from torch.utils.data import Dataset
-import gc
-from functions.create_partition import create_partition_func_1nn, create_partition_func_grid
+
 from functions.no_bn import bn_track_stats
 
 
@@ -45,13 +44,13 @@ class Buffer(Dataset):
             assert n_tasks is not None
             self.task_number = n_tasks
             self.buffer_portion_size = buffer_size // n_tasks
-        self.attributes = ['examples', 'labels', 'logits', 'clusterID', 'task_labels']
+        self.attributes = ['examples', 'labels', 'logits', 'task_labels']
         self.attention_maps = [None] * buffer_size
         self.lip_values = [None] * buffer_size
 
         self.balanced_class_perm  = None
         self.transform = None
-
+        
 
     def class_stratified_add_data(self, dataset, cpt, model=None, desired_attrs=None):
         if not hasattr(self, 'task'):
@@ -101,9 +100,6 @@ class Buffer(Dataset):
                     if hasattr(self, 'task_labels') or desired_attrs is not None and 'task_labels' in desired_attrs:
                         add_dict['task_labels'] = (torch.ones(len(not_aug_inputs)) *
                                                     (self.task))[flags]
-                    if hasattr[self, 'clusterID'] or desired_attrs is not None and 'clusterID' in desired_attrs:
-                        partition_func = create_partition_func_1nn((84, 84, 3), n_centroids=5000)
-                        add_dict['clusterID'] = partition_func(not_aug_inputs[flags])
                     self.add_data(**add_dict)
         self.task += 1
 
@@ -130,7 +126,7 @@ class Buffer(Dataset):
             transform = lambda x: x
         else:
             transform = self.transform
-        gc.collect(generation = 2)
+
         inp = self.examples[index]
         ret_tuple = (transform(inp).to(self.device), inp)
         for attr_str in self.attributes[1:]:
@@ -142,7 +138,7 @@ class Buffer(Dataset):
 
 
     def init_tensors(self, examples: torch.Tensor, labels: torch.Tensor,
-                     logits: torch.Tensor, clusterID: torch.Tensor ,task_labels: torch.Tensor) -> None:
+                     logits: torch.Tensor, task_labels: torch.Tensor) -> None:
         """
         Initializes just the required tensors.
         :param examples: tensor containing the images
@@ -157,52 +153,40 @@ class Buffer(Dataset):
                 setattr(self, attr_str, torch.zeros((self.buffer_size,
                         *attr.shape[1:]), dtype=typ, device=self.device))
 
-    def add_data(self, examples, labels=None, logits=None, task_labels=None, clusterID = None, attention_maps=None, lip_values=None):
+    def add_data(self, examples, labels=None, logits=None, task_labels=None, attention_maps=None, lip_values=None):
         """
         Adds the data to the memory buffer according to the reservoir strategy.
         :param examples: tensor containing the images
         :param labels: tensor containing the labels
         :param logits: tensor containing the outputs of the network
         :param task_labels: tensor containing the task labels
-        :return: List of indices where the data was added
+        :return:
         """
         if not hasattr(self, 'examples'):
-            self.init_tensors(examples, labels, logits, clusterID, task_labels)
+            self.init_tensors(examples, labels, logits, task_labels)
 
         rix = []
-
         for i in range(examples.shape[0]):
             index = reservoir(self.num_seen_examples, self.buffer_size)
             self.num_seen_examples += 1
-
             if index >= 0:
                 if self.examples.device != self.device:
                     self.examples.to(self.device)
                 self.examples[index] = examples[i].to(self.device)
-                
                 if labels is not None:
                     if self.labels.device != self.device:
                         self.labels.to(self.device)
                     self.labels[index] = labels[i].to(self.device)
-
                 if logits is not None:
                     if self.logits.device != self.device:
                         self.logits.to(self.device)
                     self.logits[index] = logits[i].to(self.device)
-
                 if task_labels is not None:
                     if self.task_labels.device != self.device:
                         self.task_labels.to(self.device)
                     self.task_labels[index] = task_labels[i].to(self.device)
-
-                if clusterID is not None:
-                    if self.clusterID.device != self.device:
-                        self.clusterID.to(self.device)
-                    self.clusterID[index] = clusterID[i].to(self.device)
-
                 if attention_maps is not None:
                     self.attention_maps[index] = [at[i].byte() for at in attention_maps]
-
                 if lip_values is not None:
                     self.lip_values[index] = [val[i].data for val in lip_values]
 
@@ -247,7 +231,7 @@ class Buffer(Dataset):
         if transform is None: transform = lambda x: x
         ret_tuple = (torch.stack([transform(ee.cpu())
                             for ee in self.examples[indexes]]).to(target_device),)
-        for attr_str in self.attributes[:-1]:
+        for attr_str in self.attributes[1:]:
             if hasattr(self, attr_str):
                 attr = getattr(self, attr_str).to(target_device)
                 ret_tuple += (attr[indexes],)
@@ -321,7 +305,7 @@ class Buffer(Dataset):
         if transform is None: transform = lambda x: x
         ret_tuple = (torch.stack([transform(ee.cpu())
                             for ee in self.examples]).to(self.device),)
-        for attr_str in self.attributes[1:4]:
+        for attr_str in self.attributes[1:]:
             if hasattr(self, attr_str):
                 attr = getattr(self, attr_str)
                 ret_tuple += (attr,)
@@ -335,107 +319,3 @@ class Buffer(Dataset):
             if hasattr(self, attr_str):
                 delattr(self, attr_str)
         self.num_seen_examples = 0
-    
-    def get_data_by_clusterID(self, clusterID, transform: transforms = None, return_index=False):
-        """
-        Returns data based on the provided clusterID.
-        :param clusterID: the clusterID to filter the data
-        :param transform: the transformation to be applied (data augmentation)
-        :param return_index: if True, return indices along with data
-        :return: Tuple of data based on the clusterID
-        """
-        target_device = self.device
-
-        cluster_indices = [i for i, cid in enumerate(self.clusterID) if cid == clusterID]
-
-        if not cluster_indices:
-            # No data with the specified clusterID found
-            return None
-
-        if return_index:
-            choice = torch.tensor(cluster_indices).to(target_device)
-            ret_tuple = (choice,)
-        else:
-            choice = torch.tensor(cluster_indices).to(target_device)
-            ret_tuple = ()
-
-        if transform is None:
-            transform = lambda x: x
-
-        ret_tuple += (torch.stack([transform(ee.cpu()) for ee in self.examples[cluster_indices]]).to(target_device),)
-
-        for attr_str in self.attributes[1:-1]:
-            if hasattr(self, attr_str):
-                attr = getattr(self, attr_str).to(target_device)
-                ret_tuple += (attr[cluster_indices],)
-        return ret_tuple
-
-    def generate_augment_data(self, mean, std, partition_func):
-        """
-        Generate augmented data for the memory buffer.
-        """
-        if not hasattr(self, 'examples'):
-            return
-
-        transform30 = transforms.Compose([
-            transforms.RandomRotation(30),
-            transforms.Normalize(mean, std),
-        ])
-        transform60 = transforms.Compose([
-            transforms.RandomRotation(60),
-            transforms.Normalize(mean, std),
-        ])
-        transform45 = transforms.Compose([
-            transforms.RandomRotation(45),
-            transforms.Normalize(mean, std),
-        ])
-        transform75 = transforms.Compose([
-            transforms.RandomRotation(75),
-            transforms.Normalize(mean, std),
-        ])
-        
-        if hasattr(self, 'examples'):
-            with torch.no_grad():
-                self.augment_examples = torch.cat([
-                    torch.stack([transform30(ee.cpu()) for ee in self.examples]),
-                    torch.stack([transform60(ee.cpu()) for ee in self.examples]),
-                    torch.stack([transform45(ee.cpu()) for ee in self.examples]),
-                    torch.stack([transform75(ee.cpu()) for ee in self.examples]),
-                ]).to(self.device)
-        
-        if hasattr(self, 'labels'):
-            with torch.no_grad():
-                self.augment_labels = torch.cat([self.labels] * 4).to(self.device)
-        
-        if hasattr(self, 'logits'):
-            self.augment_logits = None
-        
-        if hasattr(self, 'clusterID'):
-            with torch.no_grad():
-                self.augment_clusterID = partition_func(self.augment_examples)
-        
-        if hasattr(self, 'task_labels'):
-            with torch.no_grad():
-                self.augment_task_labels = torch.cat([self.task_labels] * 4).to(self.device)
-
-    def get_augment_data(self, choice):
-        """
-        Return augmented data.
-        """
-        ret_tuple = ()
-        if hasattr(self, 'augment_examples'):
-            augment_choice = torch.cat([choice,
-                                        choice + self.buffer_size,
-                                        choice + 2 * self.buffer_size,
-                                        choice + 3 * self.buffer_size])
-            ret_tuple = (self.augment_examples[augment_choice],)
-            for attr_str in ['augment_labels', 'augment_logits', 'augment_clusterID']:
-                if hasattr(self, attr_str):
-                    attr = getattr(self, attr_str)
-                    if attr is not None:
-                        ret_tuple += (attr[augment_choice],)
-                    else:
-                        ret_tuple += (attr,)
-            return ret_tuple
-        else:
-            return None

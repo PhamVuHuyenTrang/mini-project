a2a5dfc7cf11f17a44ec37cd6d785a405b5fd9d3
Huyền Trang <98087513+PhamVuHuyenTrang@users.noreply.github.com>
2023-12-03 19:51:37 +0700

    Update seq_miniimagenet.py

--- datasets/seq_miniimagenet.py
+++ datasets/seq_miniimagenet.py
@@ -136,7 +136,7 @@ class SequentialMiniImagenet(ContinualDataset):
             "minibatch_size":128,
             "scheduler":"simple",
             "scheduler_rate":0.2,
-            "n_epochs":40,
+            "n_epochs":0,
             "pre_epochs":40,
             "opt_steps":[10, 20, 35]})
             

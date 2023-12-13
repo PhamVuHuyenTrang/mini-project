7c517e2e9fba3388ce42f696c2ee566f58f2dcac
Huyền Trang <98087513+PhamVuHuyenTrang@users.noreply.github.com>
2023-12-13 21:56:43 +0700

    Delete fix directory

--- fix/main.sh
+++
@@ -1,7 +0,0 @@
-export
-
- CUDA_VISIBLE_DEVICES="0"
-export
-
- PYTHONPATH="."
-python functions/main.py --dataset=seq-miniimg --model=icarl_lipschitz --buffer_size=2000 --lr=0.1 --pre_epochs=200 --datasetS=tinyimgR --wd_reg=0 --method=localrobustness

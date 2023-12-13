03a882c8918729d4d18842656b188f02ba77b474
Huyền Trang <98087513+PhamVuHuyenTrang@users.noreply.github.com>
2023-12-13 22:02:52 +0700

    Update icarl_lipschitz.py

--- models/icarl_lipschitz.py
+++ models/icarl_lipschitz.py
@@ -347,7 +347,7 @@ class ICarlLipschitz(RobustnessOptimizer):
         return loss, output_features
 
     def begin_task(self, dataset):
-        if self.current_task > 4:
+        if self.current_task > 0:
             exit() 
         if self.current_task == 0:
             self.load_initial_checkpoint()

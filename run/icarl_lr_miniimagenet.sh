86b7315de08df133872846bc11c83a93431080cc
Huyền Trang <98087513+PhamVuHuyenTrang@users.noreply.github.com>
2023-12-13 21:57:46 +0700

    Update icarl_lr_miniimagenet.sh

--- run/icarl_lr_miniimagenet.sh
+++ run/icarl_lr_miniimagenet.sh
@@ -4,4 +4,4 @@ export
 export
 
  PYTHONPATH="."
-python functions/main.py --dataset=seq-miniimg --model=icarl_lipschitz --buffer_size=2000 --lr=0.1 --pre_epochs=200 --datasetS=tinyimgR --wd_reg=0 --method=localrobustness
+python functions/main.py --dataset=seq-miniimg --model=icarl_lipschitz --buffer_size=2000 --lr=0.1 --pre_epochs=200 --datasetS=tinyimgR --wd_reg=0 --method=localrobustness > output
